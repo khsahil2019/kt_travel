a6f147240e709c81ebf3633aa4199dc39b4810bb
khsahil2019 <77822213+khsahil2019@users.noreply.github.com>
2023-05-03 21:53:16 +0530

    commit

--- lib/Register/signup.dart
+++ lib/Register/signup.dart
@@ -46,116 +46,122 @@ class _SignupPageState extends State<SignupPage> {
       // appBar: AppBar(
       //   title: Text('Sign up'),
       // ),
-      body: Container(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(
-              height: 30,
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              GestureDetector(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: Icon(Icons.arrow_back_sharp)),
-              Text("Login")
-            ]),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              "Sign up",
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
-                // fontFamily: "Sail"
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                height: 30,
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            TextField(
-              controller: _firstNameController,
-              decoration: InputDecoration(
-                labelText: 'First name',
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(Icons.arrow_back_sharp)),
+                Text("Login")
+              ]),
+              SizedBox(
+                height: 30,
               ),
-            ),
-            // SizedBox(height: 16),
-            // TextField(
-            //   controller: _firstNameController,
-            //   decoration: InputDecoration(
-            //     labelText: 'Last name',
-            //   ),
-            // ),
-            SizedBox(height: 16),
-            TextField(
-              controller: _emailController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: 'Email',
+              Text(
+                "Sign up",
+                style: TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                  // fontFamily: "Sail"
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            TextField(
-              controller: _mobileController,
-              // obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Mobile Number',
+              SizedBox(
+                height: 30,
               ),
-            ),
-            SizedBox(height: 16),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
+              TextField(
+                controller: _firstNameController,
+                decoration: InputDecoration(
+                  labelText: 'First name',
+                ),
               ),
-            ),
+              // SizedBox(height: 16),
+              // TextField(
+              //   controller: _firstNameController,
+              //   decoration: InputDecoration(
+              //     labelText: 'Last name',
+              //   ),
+              // ),
+              SizedBox(height: 16),
+              TextField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                ),
+              ),
+              SizedBox(height: 16),
+              TextField(
+                controller: _mobileController,
+                // obscureText: true,
+                keyboardType: TextInputType.number,
+                maxLength: 10,
+                // enableSuggestions: true,
+                decoration: InputDecoration(
+                  labelText: 'Mobile Number',
+                  counterText: "",
+                ),
+              ),
+              SizedBox(height: 16),
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+              ),
 
-            SizedBox(height: 46),
-            ElevatedButton(
-              onPressed: () {
-                _submitForm();
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(builder: (context) => DetailPage()),
-                // );
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Icon(Icons.mail),
-                  //SizedBox(width: 8),
-                  Text('Create account'),
-                ],
+              SizedBox(height: 46),
+              ElevatedButton(
+                onPressed: () {
+                  _submitForm();
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => DetailPage()),
+                  // );
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Icon(Icons.mail),
+                    //SizedBox(width: 8),
+                    Text('Create account'),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            Center(child: Text("or")),
-            SizedBox(height: 16),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.grey, //background color of button
+              SizedBox(height: 16),
+              Center(child: Text("or")),
+              SizedBox(height: 16),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey, //background color of button
+                ),
+                onPressed: () {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => CategoryScreen()),
+                  // );
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.mail),
+                    SizedBox(width: 8),
+                    Text('Continue with Gmail'),
+                  ],
+                ),
               ),
-              onPressed: () {
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(builder: (context) => CategoryScreen()),
-                // );
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.mail),
-                  SizedBox(width: 8),
-                  Text('Continue with Gmail'),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
